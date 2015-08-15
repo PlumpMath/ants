4ec48d7be4869d1e2c8a0a247d978c2ade1c1427
Alex Eberts <alex.eberts@gmail.com>
2015-08-15 10:49:40 -0400

    Most basic sprite rendering working

--- src/ants/core.cljs
+++ src/ants/core.cljs
@@ -1,18 +1,47 @@
 (ns ^:figwheel-always ants.core
-    (:require))
+  (:require))
+
+;; Dev
+;; Figwheel server
+;; No routing
+;; Call pixi directly from clojurescript (i.e. no cljs wrapper)
+;; Minimal javascript (if possible)
+;;
 
 (enable-console-print!)
 
-(println "Edits to this text should show up in your developer console.")
-
 ;; define your app data so that it doesn't get over-written on reload
 
-(defonce app-state (atom {:text "Hello world!"}))
+(defonce world-state (atom {}))
 
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
   ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  )
 
+(def renderer (js/PIXI.autoDetectRenderer 400 300))
+
+(.appendChild (.-body js/document) (.-view renderer))
+
+(defonce stage (js/PIXI.Container.))
+
+(def bunnytexture (js/PIXI.Texture.fromImage "images/bunny.png"))
+
+(def bunny (js/PIXI.Sprite. bunnytexture))
+
+(set! (.-position.x bunny) 200)
+(set! (.-position.y bunny) 150)
+
+(. stage addChild bunny)
+
+(defn animate
+  []
+  (do
+    (. renderer render stage)
+    (js/requestAnimationFrame animate)
+    (set! (.-rotation bunny) (+ 0.02 (.-rotation bunny)))
+    ))
+
+(animate)
